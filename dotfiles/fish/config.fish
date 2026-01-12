4dda08f6d7afef59042f19cd017975cbe1388b08
Biko34 <carloscc87@gmail.com>
2026-01-12 11:45:30 +0100

    feat: added install.sh V2

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -2,9 +2,9 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 alias ls='eza -la --color=always --icons'
-alias nrs="cd ~/nixos-conf && git add . && sudo nixos-rebuild switch --flake .#default"
+alias nrs="cd ~/nixos-config && git add . && sudo nixos-rebuild switch --flake .#default"
 alias netejar5="sudo nix-env --profile /nix/var/nix/profiles/system --delete-generations +5"
-alias nupdate="cd ~/nixos-conf && nix flake update && nrs"
+alias nupdate="cd ~/nixos-config && nix flake update && nrs"
 starship init fish | source
 set -U fish_greeting 
 nitch
